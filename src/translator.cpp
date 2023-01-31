dbc2cc24e9225667582140ab7e021182e77f4119
sadib946 <sadib946@gmail.com>
2023-01-31 21:12:06 +0100

    exercice 2 running

--- src/translator.cpp
+++ src/translator.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <set>
+using namespace std;
+
+set<string> make_exit_commands()
+{
+    return {"q", "quit", "e", "exit"};
+}
 
 int main()
 {
+    string input;
+
     while (true)
     {
-        std::cout << "This is a translator, but it does nothing for now..." << std::endl;
-        std::cin.ignore();
+        cout << "Write something" << endl;
+        cin >> input;
+        cout << "Your worte : " << input << endl;
+        if(make_exit_commands().count(input) != 0){
+            break;
+        }
+        // cout << "This is a translator, but it does nothing for now..." << endl;
+        // cin.ignore();
     }
 
     return 0;
