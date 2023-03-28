3b24c664d449a27dae20a0410e9442ff0c296e66
sadib946 <sadib946@gmail.com>
2023-03-28 13:25:11 +0200

    tp8 not done

--- src/Kitchen.hpp
+++ src/Kitchen.hpp
@@ -1,4 +1,28 @@
 #pragma once
+#include "../lib/Unit.hpp"
+#include <set>
+
 
 class Kitchen
-{};
+{
+    public:
+
+    const Unit& register_unit(Unit unit){
+        units.insert(std::move(unit));
+        return unit;
+    }
+
+    const Unit* find_unit(const std::string& name) const
+    {
+        for (const auto& unit : units){
+            if (unit.name == name){
+                return &unit;
+            }
+        }
+        return nullptr;
+    }
+
+
+    private:
+    std::set<Unit> units;
+};
